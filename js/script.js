4bd0b052012a681e8666ec61bd650046c907bd08
Igor <iryzhikov.igor@gmail.com>
2021-02-24 21:03:47 +0200

    second commit.v1

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@
     - genres - сюда поместить пустой массив
     - privat - в это свойство поместить boolean(логическое) значение false
 
-3) Задайте пользователю по два раза вопросы:
+3) Задайте пользователю по два раза вопросы:ё
     - 'Один из последних просмотренных фильмов?'
     - 'На сколько оцените его?'
 Ответы стоит поместить в отдельные переменные
@@ -20,7 +20,6 @@
     }
 
 Проверить, чтобы все работало без ошибок в консоли */
-
 'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
@@ -33,12 +32,30 @@ const personalMovieDB = {
     privat: false
 }; 
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = +prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = +prompt('На сколько оцените его?', '');
-    
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let  i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+           b = +prompt('На сколько оцените его?', '');
+
+    if (a != null  && b != null && a != '' && b != '', a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
+
+if(personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB); 
+
+
+
